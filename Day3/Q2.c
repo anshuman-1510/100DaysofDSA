e3e881c96e1671bded69e4092136bf84c830c265
anshuman-1510 <anshumandav2007@gmail.com>
2026-04-23 01:50:24 +0530

    Completed

---
+++ Day3/Q2.c
@@ -0,0 +1,33 @@
+/*You are given an array arr[] of size n - 1 that contains distinct integers in the range from 1 to n (inclusive). This array represents a permutation of the integers from 1 to n with one element missing. Your task is to identify and return the missing element.*/
+
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    int arr[n - 1];
+
+    printf("Enter %d elements: ", n - 1);
+    for (int i = 0; i < n - 1; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Calculate total sum from 1 to n
+    int totalSum = n * (n + 1) / 2;
+
+    // Calculate array sum
+    int arrSum = 0;
+    for (int i = 0; i < n - 1; i++) {
+        arrSum += arr[i];
+    }
+
+    int missing = totalSum - arrSum;
+
+    printf("Missing number = %d\n", missing);
+
+    return 0;
+}
